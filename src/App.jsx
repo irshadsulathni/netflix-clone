647a93231397f46d186dc0b0f532dda730c39654
irshadsulthani <irshadpallar2020@gmail.com>
2025-02-25 10:24:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-black min-h-screen">
+      <h1 className="text-3xl text-red-400 font-bold underline">
+        Hello world!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
